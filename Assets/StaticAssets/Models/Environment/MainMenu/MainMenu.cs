d39230fb9be9cfb7001a7ac9f5f90811fe625305
orceN2k <dominic.schuermann@bluewin.ch>
2015-03-02 15:26:34 +0100

    add default level sprites and camera script

---
+++ Assets/StaticAssets/Models/Environment/MainMenu/MainMenu.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+		
+	public void ChangeToScene(string sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
